030ddc9e5ea5d138434f4b5388682aaee64c03dc
Mateus Nóbrega <mateusn@outlook.com>
2018-06-02 01:49:03 -0300

    Started refactoring

--- edit_distance.cpp
+++ edit_distance.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <stack>
 #include <sstream>
+#include <cstring>
 
+#define STR_LIMIT 5000
 //matrix size limits
 #define MAX_M 1000
 #define MAX_N 1000
@@ -23,10 +25,10 @@ using std::string;
 
 /*
     Bottom-up implementation returning a string with the operations.
-    Strongly based on https://github.com/edsomjr/TEP/blob/master/Strings/text/Programacao_Dinamica.md 
-    with some improvements on readability and minor tweaks.
+    Strongly based on @edsomjr's implementation with some improvements 
+    on readability and minor tweaks.
 */
-string edit_distance(const string& str_1, const string& str_2)
+int edit_distance(const string& str_1, const string& str_2)
 {
     int m = str_1.size();
     int n = str_2.size();
@@ -61,37 +63,39 @@ string edit_distance(const string& str_1, const string& str_2)
                 operation[i][j] = 's';
         }
 
-    std::cout << "\nThe edit cost between " << str_1 << " and " << str_2 << " is " << state[m][n] << ".\n";
+        return state[m][n];
+}
 
-    int i = m, j = n;
+string show_operations(const string& str_1, const string& str_2)
+{
+    int i = str_1.size(), j = str_2.size();
     std::stack<string> S;
-    char buffer[128];
-    string subs = "[x->y]";
+    char operations_buffer[STR_LIMIT];
 
-    while (i or j)
+    while (i != 0 or j != 0)
     {
         switch (operation[i][j]) {
-        case 'i':
-            sprintf(buffer, "%c", str_2[j - 1]);
-            --j;
-            break;
+            case 'i':
+                sprintf(operations_buffer, "add \"%c\" to index %d\n", str_2[j-1], (j-1));
+                j--;
+                break;
 
-        case 'r':
-            sprintf(buffer, "x");
-            --i;
-            break;
+            case 'r':
+                sprintf(operations_buffer, "remove \"%c\"\n", str_1[i-1]);
+                i--;
+                break;
 
-        case 's':
-            if (str_1[i-1] == str_2[j-1])
-                sprintf(buffer, "-");
-            else
-                sprintf(buffer, "[%c->%c]", str_1[i - 1], str_2[j - 1]);
+            case 's':
+                if (str_1[i-1] == str_2[j-1])
+                    sprintf(operations_buffer, "keep");
+                else
+                    sprintf(operations_buffer, "swap \"%c\" to \"%c\"\n", str_1[i-1], str_2[j-1]);
+                
+                i--;j--;
+            }
 
-            --i;
-            --j;
-        }
-
-        S.push(buffer);
+            if (strncmp(operations_buffer, "keep", 5) != 0)
+                S.push(operations_buffer);
     }
 
     std::ostringstream os;
@@ -111,8 +115,8 @@ int main()
     std::cout << "Insert two strings in the format \"str1 str2\": ";
     std::cin >> str_1 >> str_2;
 
-    string result = edit_distance(str_1, str_2);
+    int result = edit_distance(str_1, str_2);
 
-    std::cout << "\nThe edit script is: " << result << std::endl;
-
+    std::cout << "\nThe edit distance is: " << result << std::endl;
+    std::cout << "\nThe edit script is: \n" << show_operations(str_1, str_2) << std::endl;
 }
